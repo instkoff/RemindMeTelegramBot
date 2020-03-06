15272b2fd98f55b9d5e204362f52f5d89ffb1876
Larion Starikov <i.n.st.koff@gmail.com>
2020-03-06 14:22:34 +0700

    Начало

--- RemindMeTelegramBotv2/Services/UpdateService.cs
+++ RemindMeTelegramBotv2/Services/UpdateService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using RemindMeTelegramBotv2.Models;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -8,10 +9,12 @@ namespace RemindMeTelegramBotv2.Services
     public class UpdateService : IUpdateService
     {
         private readonly IBotClient _botClient;
+        private readonly ILogger<UpdateService> _logger;
 
-        public UpdateService(IBotClient botClient)
+        public UpdateService(IBotClient botClient, ILogger<UpdateService> logger)
         {
             _botClient = botClient;
+            _logger = logger;
         }
         public async Task AnswerAsync(Update update)
         {
@@ -19,6 +22,7 @@ namespace RemindMeTelegramBotv2.Services
                 return;
 
             var message = update.Message;
+            _logger.LogInformation("Received Message from {0}", message.Chat.Id);
 
             if (message.Type == MessageType.Text)
             {
