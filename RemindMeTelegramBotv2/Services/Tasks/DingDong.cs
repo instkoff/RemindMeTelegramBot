3a0633b12acce7cb7afbc05c84f2f41792d39d29
Larion Starikov <i.n.st.koff@gmail.com>
2020-04-04 21:17:27 +0700

    ПРикручиваю Quartz

---
+++ RemindMeTelegramBotv2/Services/Tasks/DingDong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+using RemindMeTelegramBotv2.DAL;
+using RemindMeTelegramBotv2.Models;
+
+namespace RemindMeTelegramBotv2.Services.Tasks
+{
+    public class DingDong : IJob
+    {
+        private readonly IBotClient _botClient;
+        private readonly IDbRepository<RemindEntity> _dbRepository;
+        public DingDong(IBotClient botClient, IDbRepository<RemindEntity> dbRepository)
+        {
+            _botClient = botClient;
+            _dbRepository = dbRepository;
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var now = DateTime.Now.ToUniversalTime();
+            if (FillRemindsList._currentReminds.Count > 0)
+            {
+                foreach (var remind in FillRemindsList._currentReminds)
+                {
+                    if (remind.EndTime <= now)
+                    {
+                        await _botClient.Client.SendTextMessageAsync(remind.TelegramChatId, remind.RemindText);
+                        remind.SetState(RemindEntity.State.Completed);
+                        _dbRepository.Update(remind.Id, remind);
+                    }
+                }
+                FillRemindsList._currentReminds.RemoveAll(r => r.state == RemindEntity.State.Completed);
+            }
+        }
+    }
+}
--- RemindMeTelegramBotv2/Services/Tasks/FillRemindsList.cs
+++ RemindMeTelegramBotv2/Services/Tasks/FillRemindsList.cs
@@ -10,13 +10,11 @@ namespace RemindMeTelegramBotv2.Services.Tasks
     public class FillRemindsList : IJob
     {
         private readonly IDbRepository<RemindEntity> _dbRepository;
-        private readonly IBotClient _botClient;
-        static List<RemindEntity> _currentReminds;
+        public static List<RemindEntity> _currentReminds;
 
-        public FillRemindsList(IDbRepository<RemindEntity> dbRepository, IBotClient botClient)
+        public FillRemindsList(IDbRepository<RemindEntity> dbRepository)
         {
             _dbRepository = dbRepository;
-            _botClient = botClient;
             _currentReminds = new List<RemindEntity>();
         }
         public async Task Execute(IJobExecutionContext context)
@@ -33,6 +31,20 @@ namespace RemindMeTelegramBotv2.Services.Tasks
             }
             _currentReminds.Sort((a, b) => a.EndTime.CompareTo(b.EndTime));
             _dbRepository.RemoveMany(r => r.state == RemindEntity.State.Completed);
+        }
+
+        public void TryAddToRemindsSequence(object obj)
+        {
+            var now = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Utc);
+            var nowPlusDay = TimeZoneInfo.ConvertTime(DateTime.Now.AddDays(1), TimeZoneInfo.Utc);
+            if (obj != null)
+            {
+                var remind = (RemindEntity)obj;
+                if (remind.EndTime <= nowPlusDay && remind.EndTime >= now)
+                {
+                    _currentReminds.Add(remind);
+                }
+            }
         }
     }
 }
