fd137c49e1091f432c962a59ecfc5c9cde227293
Larion Starikov <i.n.st.koff@gmail.com>
2020-04-06 18:14:28 +0700

    123

--- RemindMeTelegramBotv2/Models/Commands/MyRemindsListCommand.cs
+++ RemindMeTelegramBotv2/Models/Commands/MyRemindsListCommand.cs
@@ -14,12 +14,10 @@ namespace RemindMeTelegramBotv2.Scheduler.Commands
         public override async Task ExecuteAsync(TelegramBotClient botClient, MessageInfo message,
             IDbRepository<RemindEntity> remindRepository)
         {
-            //await botClient.SendTextMessageAsync(message.ChatId, "Введите ваш часовой пояс:\n (Например: +7)");
             var remindsList = await remindRepository.GetListAsync(r => r.TelegramUsernameId == message.FromId && r.state != RemindEntity.State.Completed);
             if (remindsList.Count != 0)
             {
                 StringBuilder remindsBuilder = new StringBuilder();
-                //TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById();
                 remindsList.ForEach(r => { remindsBuilder.Append($"{r.EndTime} напомнить о: {r.RemindText} \n"); });
                 await botClient.SendTextMessageAsync(message.ChatId, remindsBuilder.ToString());
                 IsComplete = true;
